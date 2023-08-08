156463ddb03f59962fae86a92dfda924b24b5050
Juhász Csenge <csenge799@gmail.com>
2023-08-08 21:26:19 +0200

    Create Nextjs app

---
+++ app/Components/AddTask.tsx
@@ -0,0 +1,12 @@
+const AddTask = ( ) => {
+    return(
+    <div>
+        <button className="btn btn-primary w-full">Add new Task</button>
+    </div>
+    
+    );
+        
+    
+};
+
+export default AddTask;
