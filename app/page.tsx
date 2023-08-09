0745512002eb916ffa829ca907e7ff7911e60819
Juhász Csenge <csenge799@gmail.com>
2023-08-09 20:33:08 +0200

    Nextjs todo app befejezése

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
+import { getAllTodos } from '@/api';
 import AddTask from './Components/AddTask'
+import TodoList from './Components/TodoList'
 import './globals.css'
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+
+
   return (
     <main className='max-w-4xl mx-auto mt-4'>
      <div className='text-center my-5 flex-col gap-4'>
-      <h1 className='text-2xl font-bold'>Todo List App</h1>
+      <h1 className='text-2xl font-bold'>Feladat kezelő</h1>
+      <AddTask />
      </div>
-     <AddTask />
+     <TodoList tasks={tasks} />
     </main>
   )
 }
